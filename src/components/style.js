8f35053d612011873766f5b029b4ef980cc1d4fe
Jemimah Sai <jemimah.beryl.sai@gmail.com>
2021-10-15 18:10:39 +0800

    Add sample drag and drop functionality

---
+++ src/components/style.js
@@ -0,0 +1,16 @@
+export const listStyle = {
+                            display: "flex",
+                            flex: "row wrap",
+                            justifyContent: "center",
+                            padding: "15px 30px",
+                            backgroundColor: "yellow"
+                        };
+export const listItemStyle = {
+                                width: "80px",
+                                margin: "0 8px",
+                                border: "1px solid black",
+                                fontSize: "24px",
+                                padding: "8px 16px",
+                                textAlign: "center",
+                                cursor: "all-scroll"
+                            };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
+import {List} from './components/List'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 const App = () => {
-  return <div>
-      <h1>Hello World!</h1>
-  </div>
+  return <DndProvider backend={HTML5Backend}>
+  <List />
+</DndProvider>
 };
 
 ReactDOM.render(<App />, document.getElementById("app"));
